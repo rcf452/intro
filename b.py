6f1da87f47c5a599ef82562f6098434935ce4bbc
Kim Yeonji <rcf452@gmail.com>
2019-04-03 19:43:34 +0900

    intro commit

---
+++ b.py
@@ -0,0 +1,48 @@
+# print("hello world")
+# a = "abc"
+# print(a)
+
+# print(10)
+
+# print("10")
+
+# print(type(10.0))
+
+# print(type("10"))
+
+
+# b=10
+# c=3
+# print(b/c)
+# print(b%c)
+
+# print('""')
+
+# print("\"\"")
+
+# print("\n")
+
+# e="hello"
+# f="world"
+
+# print(e+f)
+
+# print(e*5)
+
+# h="hello world"
+
+# print(h[5])
+
+# print(h[:-1])
+# print(len(h))
+# print(h.count("o"))
+# print(str(h.index("l"))+"\n")
+# """print(h.find("l"))
+
+# x=h.replace('l','k')"""
+#print(x)
+
+a = 10
+b = int(input())
+
+print(b)
